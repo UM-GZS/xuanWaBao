8116e69ebf882996b1c118bf46d435a7bdd8dfa5
徐健飞 <1483860490@qq.com>
2021-07-04 17:39:44 +0800

    编写我的页面，个人中心页面

--- pages/user/user.vue
+++ pages/user/user.vue
@@ -1,6 +1,51 @@
 <template>
 	<view class="user_wrap">
-		我的界面
+		
+		<!-- 用户信息 -->
+		<view class="user_info">
+			<view class="left_info">
+				<text class="name">用户名称</text>
+				<text class="phone">12345678900</text>
+			</view>
+			<view class="right_cover">
+				<image src="../../static/index/sw1.jpg" mode=""></image>
+			</view>
+		</view>
+		<!-- 发布和收藏 -->
+		<view class="send_collection_wrap">
+			<view class="send_collection_ctrl">
+					
+				<view class="">
+					<image src="../../static/uview/common/favicon.ico" mode=""></image>
+					<text>我的发布</text>
+				</view>
+				<view>
+					<image src="../../static/uview/common/favicon.ico" mode=""></image>
+					<text>我的收藏</text>
+				</view>
+			</view>
+		</view>
+		<!-- 按钮控制 -->
+		<view class="btn_control">
+			<u-cell-group>
+				<u-cell-item   title="个人中心" :arrow="false">
+					<u-image width="40rpx" height="40rpx" style="margin-right: 15rpx;" src="/static/uview/common/favicon.ico" slot="icon"></u-image>
+				</u-cell-item>
+				<u-cell-item  title="我的车辆" :arrow="false">
+					<u-image width="40rpx" height="40rpx" style="margin-right: 15rpx;" src="/static/uview/common/favicon.ico" slot="icon"></u-image>
+				</u-cell-item>
+				<u-cell-item  title="认证信息" :arrow="false">
+					<u-image width="40rpx" height="40rpx" style="margin-right: 15rpx;" src="/static/uview/common/favicon.ico" slot="icon"></u-image>
+				</u-cell-item>
+				<u-cell-item  title="收货地址" :arrow="false">
+					<u-image width="40rpx" height="40rpx" style="margin-right: 15rpx;" src="/static/uview/common/favicon.ico" slot="icon"></u-image>
+				</u-cell-item>
+				<u-cell-item  title="联系客服" :arrow="false">
+					<u-image width="40rpx" height="40rpx" style="margin-right: 15rpx;" src="/static/uview/common/favicon.ico" slot="icon"></u-image>
+				</u-cell-item>
+			</u-cell-group>
+		</view>
+		
 	</view>
 </template>
 
@@ -8,6 +53,7 @@
 	export default {
 		data() {
 			return {
+				
 			}
 		},
 		methods: {
@@ -18,5 +64,74 @@
 	}
 </script>
 
-<style>
+<style lang="scss">
+	page{
+		background-color: #FFFFFF;
+	}
+	.user_wrap{
+		width: 100vw;
+		// 用户信息
+		.user_info{
+			width: 100%;
+			height: 300rpx;
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+			padding:30rpx 80rpx 70rpx 80rpx;
+			background-color: $page_color;
+			border-bottom-left-radius: 50rpx;
+			border-bottom-right-radius: 50rpx;
+			.left_info{
+				display: flex;
+				flex-direction: column;
+				.name{
+					font-weight: 600;
+					font-size: 30rpx;
+				}
+				.phone{
+					padding-top: 10rpx;
+					font-size: 30rpx;
+				}
+			}
+			.right_cover{
+				image{
+					width: 110rpx;
+					height: 110rpx;				
+					border-radius: 50%;
+				}
+			}
+		}
+		// 发布和收藏
+		.send_collection_wrap{
+			width: 100%;
+			margin-top: -70rpx;
+			@include flex-center;
+			.send_collection_ctrl{
+				width: 500rpx;
+				padding: 30rpx 50rpx;
+				background-color: #FFFFFF;
+				box-shadow: #dddddd 0px 0px 20rpx;
+				@include flex-jcsb;
+				view{
+					@include flex-col;
+					align-items: center;
+					image{
+						width: 60rpx;
+						height: 60rpx;
+						padding-bottom: 20rpx;
+					}
+				}
+			}
+			
+		}
+		// 按钮操作
+		.btn_control{
+			padding-top: 50rpx;
+			/deep/ .u-border-bottom:after, .u-border-left:after, .u-border-right:after, .u-border-top-bottom:after, .u-border-top:after, .u-border:after {
+				border-bottom: 5rpx solid #e3e3e3;
+			}
+		}
+		
+		
+	}
 </style>
---
+++ pages/user/user_tenter.vue
@@ -0,0 +1,115 @@
+<template>
+<view class="user_tenter_wrap">
+	
+	<view class="cover_wrap">
+		<image class="cover" src="/static/index/sw1.jpg" mode=""></image>
+	</view>
+	<!-- 操作行 -->
+	<view class="user_cell">
+		
+		<view class="cell_row">
+			<view class="label">用户名称:</view>
+			<view class="content">请输入用户名称</view>
+		</view>
+		<view class="cell_row">
+			<view class="label">手机号:</view>
+			<view class="content">请输入用户名称</view>
+		</view>
+		<view class="cell_row">
+			<view class="label">企业身份:</view>
+			<view class="content" @click="show_company=true">
+				{{companyTitle}}
+			</view>
+		</view>
+		<view class="cell_row">
+			<view class="label">个人身份:</view>
+			<view class="content" @click="user_show=true">
+				{{userTitle}}
+			</view>
+		</view>
+		
+		<u-cell-group>
+			<u-cell-item icon="none" title="购买记录"  :border-top="false"></u-cell-item>
+			<u-cell-item icon="none" title="求职记录"  :border-top="false"></u-cell-item>
+			<u-cell-item icon="none" title="维修记录"  :border-top="false"></u-cell-item>
+		</u-cell-group>
+		<u-picker v-model="show_company" mode="selector" :range="company_options" @confirm="companyClick"></u-picker>
+		<u-picker v-model="user_show" mode="selector" :range="user_options" @confirm="userClick"></u-picker>
+		
+ </view>
+	
+	
+</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+				
+				show_company:false,
+				user_show:false,
+				company_options: ['文员', '技工', '管理','销售'],
+				user_options: ['个人身份1', '个人身份2', '个人身份3','个人身份4'],
+				companyTitle:"身份",
+				userTitle:"身份"
+			}
+		},
+		methods: {
+			companyClick(e){
+				let value = this.company_options[e[0]]
+				this.companyTitle = value
+			},
+			userClick(e){
+				let value = this.user_options[e[0]]
+				this.userTitle = value
+			},
+		},
+	}
+</script>
+
+<style lang="scss">
+	page{
+		background-color: #FFFFFF;
+		width: 100vw;
+	}
+	// 头像
+	.cover_wrap{
+		width: 100%;
+		padding: 50rpx 0;
+		@include  flex-center;
+		.cover{
+			border-radius: 50%;
+			width:120rpx;
+			height:120rpx
+		}
+	}
+	// 信息
+	.user_cell{
+		.cell_row{
+			padding: 40rpx 50rpx;
+			border-bottom: 4rpx solid #e3e3e3;
+			display: flex;
+			.label{
+				font-weight: 600;
+				color: #606266;	
+				padding-right: 30rpx;
+				min-width: 150rpx !important;
+			}
+			.content{
+				flex: 1;
+				color: #d0d0d0;
+			}
+		}
+		/deep/ .u-border-bottom:after, .u-border-left:after, .u-border-right:after, .u-border-top-bottom:after, .u-border-top:after, .u-border:after {
+			border-bottom: 8rpx solid #e3e3e3;
+		}
+		/deep/ .u-cell_title{
+			font-weight: 600;
+			color: #606266;	
+		}
+	}
+		
+		
+</style>
